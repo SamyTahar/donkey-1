b0d71e07f267ac086cae7ad4ab3909f8360839e4
NITESCU Cristian <az02289@FRLA000112.local>
2018-07-16 10:12:33 +0200

    Use color images

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -38,10 +38,12 @@ class PiCamera(BaseCamera):
         time.sleep(2)
 
     def preprocess(self, array):
-        img = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        lab = cv2.cvtColor(array, cv2.COLOR_BGR2LAB)
+        lab_planes = cv2.split(lab)
         clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        img = clahe.apply(img)
-        return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        lab_planes[0] = clahe.apply(lab_planes[0])
+        lab = cv2.merge(lab_planes)
+        return cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
 
     def run(self):
         f = next(self.stream)
